f9f6b66375a6092b107c4b6b666afe4ddeca8e21
Steven Y <stevenyildirim@gmail.com>
2022-01-05 22:19:18 -0500

    1. Pair with target sum

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,23 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    println("Find the pair which adds up to a sum:")
+    println("${pairWithTargetSum(6, intArrayOf(1,2,3,4,6))} expected [1,3]")
+    println("${pairWithTargetSum(11, intArrayOf(2,5,9,11))} expected [0,2]")
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+/**
+ * Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target.
+ * Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
+ */
+fun pairWithTargetSum(target: Int, arr: IntArray): Pair<Int,Int> {
+
+    var indexStart = 0
+    var indexEnd = arr.size-1
+    while(arr[indexEnd] + arr[indexStart] != target) {
+        val sum = arr[indexEnd] + arr[indexStart]
+        if(sum > target) indexEnd--
+        else indexStart++
+
+        if(indexStart == indexEnd) return Pair(-1,-1)
+    }
+    return Pair(indexStart,indexEnd)
 }
